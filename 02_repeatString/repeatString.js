779240ecbdc8246d8285ee1d622460d5c155fa13
Humam <hu666mam@gmail.com>
2022-10-27 20:09:44 +0100

    waw

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let blabla = "" ;
+    let i = 1 ;
+    while ( i <= num ) {
+        if ( num < 0 ) {
+            blabla = "ERROR"
+        } else
+        blabla += string ;
+        i++ ;
+    }
+    return blabla ;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array1, ...args) {
 
-};
-
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
