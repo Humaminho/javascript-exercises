a905a7984d67213cff77cc0da0b166c8c69b4eda
Humam <hu666mam@gmail.com>
2022-11-02 12:28:56 +0100

    fibonacci

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => total + number,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => total * number,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a){
+  let array=[]
+  for (let x=a ; x>=1 ; x--) {
+    array.push(x);
+  } return array.reduce((total, number) => total * number,1);
 };
 
 // Do not edit below this line
@@ -31,3 +34,12 @@ module.exports = {
   power,
   factorial
 };
+
+
+function factoriall(a) {
+  let array=[]
+  for (let x=a ; x>=1 ; x--) {
+    array.push(x);
+  } return array.reduce((total, number) => total * number,1);
+};
+factoriall(4)
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(numAt) {
+    if (numAt < 0 ) {
+        return "OOPS";
+    } else {
+        let array=[];
+    for (let i = 1; i<=numAt ; i++) {
+        if (isNaN(array[i-3]) && isNaN(array[i-2])) {
+            array.push(i);
+        } else if (isNaN(array[i-3])) {
+            array.push(array[i-2]);
+        } else array.push(array[i-2]+array[i-3])
+    };
+    return array[numAt-1];
+    };
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
